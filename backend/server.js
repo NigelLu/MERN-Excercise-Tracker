89994da5b3aac204a3a5b04a305b3a5880352993
NigelLu <nigellu@outlook.com>
2022-03-25 02:08:05 -0400

    Created router for users called users.js and updated format for js files

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+// modules
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+// pull config
+require("dotenv").config();
 
+// config app
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb://localhost/mern';
+// database
+const uri = "mongodb://localhost/mern";
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log(`MongoDB database connection established successfully at URI ${uri}`);
+connection.once("open", () => {
+	console.log(
+		`MongoDB database connection established successfully at URI ${uri}`
+	);
 });
 
+// load routers
+const exerciseRouter = require("./routes/exercises");
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
+app.use("/exercises", exerciseRouter);
+
+// start app
 app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
+	console.log(`Server started on port: ${port}`);
 });

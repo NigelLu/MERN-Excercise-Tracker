73b9d074c99c5fec4cb8343c5808e7beda988e44
NigelLu <nigellu@outlook.com>
2022-03-25 01:20:52 -0400

    Added .env and created user & exercise model/schema

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = 'mongodb://localhost/mern';
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log(`MongoDB database connection established successfully at URI ${uri}`);
+});
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);

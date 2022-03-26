45c3149fadb5be0b9fb345c922b30c9e26ab7665
NigelLu <nigellu@outlook.com>
2022-03-25 23:16:56 -0400

    Fix components bug

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 // config app
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
